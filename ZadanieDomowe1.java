b56aa8c14b2526846757f2c9bcc9ffe100dbbac0
Marta Chejduk <chejdukmarta@gmail.com>
2021-08-05 09:18:41 +0200

    changes

--- ZadanieDomowe1.java
+++ ZadanieDomowe1.java
@@ -1,19 +1,18 @@
 public class ZadanieDomowe1 {
     public static void main(String[] args) {
-        int humanAge = 11;
+        int humanAge = 21;
 
         if (humanAge < 0) return;
         if (humanAge > 130) return;
 
         if (humanAge < 18) {
             System.out.println("Osoba jest dzieckiem");
-        }
-        if (humanAge >= 18 && humanAge < 40) {
+        } else if (humanAge >= 18 && humanAge < 40) {
             System.out.println("Osoba jest młoda");
-        }if (humanAge >= 40 && humanAge <65) {
+        } else if (humanAge >= 40 && humanAge < 65) {
             System.out.println("Osoba jest w średnim wieku");
-        } if (humanAge >= 65) {
+        } else if (humanAge >= 65) {
             System.out.println("Osoba jest starsza");
         }
     }
-    }
+}
